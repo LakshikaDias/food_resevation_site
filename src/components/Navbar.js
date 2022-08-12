590cb9d2b38aff1216899cf5a1fc718e6f923343
LakshikaDias <lakshika9504@gmail.com>
2022-08-12 12:40:05 +0530

    completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { AppBar } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Icon,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Box, Container } from "@mui/system";
-import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   const pages = ["Customer", "Cook", "Manager"];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    // console.log(event.currentTarget);
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <div>
       <AppBar>
         <Container>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((pages, id) => {
-              return (
-                <Button
-                  key={id}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {pages}
-                </Button>
-              );
-            })}
-
-            <Button>LogIn/SignUp</Button>
-          </Box>
+          <Toolbar disableGutters>
+            <IconButton
+              size="large"
+              color="inherit"
+              onClick={handleOpenNavMenu}
+            >
+              <MenuIcon
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page, id) => {
+                return (
+                  <MenuItem key={id} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                );
+              })}
+            </Menu>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page, id) => {
+                return (
+                  <Button key={id} sx={{ color: "white", display: "block" }}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </Button>
+                );
+              })}
+            </Box>
+            <Box>
+              <Button
+                sx={{ color: "white", display: { xs: "none", md: "flex" } }}
+              >
+                <Typography> LogIn / SignUp</Typography>
+              </Button>
+            </Box>
+          </Toolbar>
         </Container>
       </AppBar>
     </div>
